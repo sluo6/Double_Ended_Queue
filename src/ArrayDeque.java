49f7c562dd6176f9073f302cd2d88cd0bdd3f218
sluo6 <shangwenluo6@gmail.com>
2017-04-11 19:03:55 -0500

    Just finished LinkedListDeque

--- src/ArrayDeque.java
+++ src/ArrayDeque.java
@@ -1,4 +1,52 @@
 
-public class ArrayDeque {
+public class ArrayDeque implements Deque{
+
+	@Override
+	public void addFirst(Object x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addLast(Object x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void printDeque() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
