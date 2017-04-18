759ffe902534dce4007ccac626e5c03eaa4bb720
sluo6 <shangwenluo6@gmail.com>
2017-04-17 19:09:21 -0500

    modified ArrayDeque. Everything works except reSize

---
+++ src/ArrayDequeTest.java
@@ -0,0 +1,24 @@
+
+public class ArrayDequeTest {
+
+	public static void main(String[] args) {
+		ArrayDeque<String> test = new ArrayDeque<String>();
+		test.addFirst("firstt");
+		test.addFirst("second");
+		test.addLast("third");
+		test.addLast("fourth");
+		test.removeLast();
+		test.addFirst("f");
+		test.addFirst("a");
+		test.addFirst("b");
+		test.addFirst("c");
+		test.addFirst("d");
+		test.addFirst("e");
+		//String output = test.get(1);
+	//	int nextfirst = test.nextLast;
+		test.printDeque();
+		//System.out.println(output);
+//		System.out.println(test.size());
+	}
+
+}
